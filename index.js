8c38a03c7c38f2e5fbcf2494bcd28fcf415add4d
Odilon Faustino <odilonfaustino@gmail.com>
2020-10-29 15:16:17 -0300

    Configuração da porta sendo recebido pela variável de ambiente

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const options = {
     useUnifiedTopology: true
 };
 
-//const client = await mongodb.MongoClient.connect(connectionString, options);
+// const client = await mongodb.MongoClient.connect(connectionString, options);
 
 console.info('MongoDB conectado com sucesso!');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Precisamos avisar o Express para utilizar o body-parser
 // Assim, ele saberá como transformar as informações no BODY da requisição
@@ -57,10 +57,9 @@ Descrição: Remove mensagem específica pelo ID
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
-
-// const db = client.db('ocean_backend_27_10_2020');
-// const mensagens = db.collection('mensagens');
 /*
+const db = client.db('ocean_backend_27_10_2020');
+const mensagens = db.collection('mensagens');
 
 // Read all
 app.get('/mensagem', async function (req, res) {
